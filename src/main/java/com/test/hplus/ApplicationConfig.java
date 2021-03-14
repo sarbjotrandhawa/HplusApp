6ed51eb0f36d9250cf4a2e2e13ec7c13d8d03dda
macbookair <macbookair@10.0.0.190>
2021-03-14 18:48:21 -0400

    Introducing L10n and I18n and theme resolution.

--- src/main/java/com/test/hplus/ApplicationConfig.java
+++ src/main/java/com/test/hplus/ApplicationConfig.java
@@ -1,19 +1,30 @@
 package com.test.hplus;
 
+import java.util.Locale;
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.ThemeResolver;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.theme.CookieThemeResolver;
+import org.springframework.web.servlet.theme.ThemeChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
 import com.test.hplus.converters.StringToEnumConvertor;
 import com.test.hplus.interceptors.LoggingInterceptor;
+
+
 
 @Configuration
 @ComponentScan(basePackages = "com.test.hplus")
@@ -55,5 +66,24 @@ public class ApplicationConfig extends WebMvcConfigurationSupport {
 	@Override
 	protected void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new LoggingInterceptor()).addPathPatterns("/*");
+		registry.addInterceptor(new ThemeChangeInterceptor());//default query param theme:
+		registry.addInterceptor(new LocaleChangeInterceptor());//default query: locale
 	}
+	
+	
+	 @Bean
+	    public ThemeResolver themeResolver() {
+	        CookieThemeResolver cookieThemeResolver = new CookieThemeResolver();
+	        cookieThemeResolver.setCookieName("theme");
+	        cookieThemeResolver.setDefaultThemeName("client-theme1");
+	        return cookieThemeResolver;
+	    }
+	 
+	 @Bean
+	    public org.springframework.web.servlet.LocaleResolver localeResolver(){
+	        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+	        cookieLocaleResolver.setDefaultLocale(Locale.US);
+	        cookieLocaleResolver.setCookieName("locale");
+	        return cookieLocaleResolver;
+	    }
 }
